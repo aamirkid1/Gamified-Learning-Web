7a07bac8e4ecb98dd23e1a640e4e2a3f30b254e0
Mohd Aaqib <aamirkid1@gmail.com>
2026-09-24 11:26:01 +0530

    fixed main.ts for redeployment on render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,7 @@
+import * as dns from 'dns';
+
+dns.setDefaultResultOrder('ipv4first');
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
